bad4993697b32e438bbcd7eb6931414cc32dc31c
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-02-16 12:33:11 -0500

    DA all in unprocessed

--- processor.py
+++ processor.py
@@ -4,7 +4,7 @@ from os import listdir
 from os.path import isfile, join
 
 
-onlyfiles = [f for f in listdir('processed/') if isfile(join('processed/', f))]
+onlyfiles = [f for f in listdir('unprocessed/') if isfile(join('unprocessed/', f))]
 
 print(onlyfiles)
 
@@ -12,8 +12,8 @@ for f in onlyfiles:
     f_no_extension = f.split(".")[0]
     string_command = "ffmpeg -i original/{} processed/{}.mp3"
     print(string_command, "String")
-    command = f"ffmpeg -i processed/{f} unprocessed/{f_no_extension}.wav"
+    command = f"ffmpeg -i unprocessed/{f} unprocessed/{f_no_extension}.wav"
     print(command)
     os.system(command)
-    delete_command = f"rm processed/{f_no_extension}.wav"
-    os.system(delete_command)
+    # delete_command = f"rm unprocessed/{f_no_extension}.wav"
+    # os.system(delete_command)
