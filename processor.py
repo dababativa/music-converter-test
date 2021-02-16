548c3c4ba21de0f4c87a9ee466d2b25ef5d6837a
Daniel Babativa <da.babativa@uniandes.edu.co>
2021-02-16 12:06:07 -0500

    DA: Multiple folders and only files

--- processor.py
+++ processor.py
@@ -1,5 +1,13 @@
 import os
 import sys
+from os import listdir
+from os.path import isfile, join
 
-command = f"ffmpeg -i original/wevatest.wav processed/all-my-life.mp3"
-os.system(command)
+
+onlyfiles = [f for f in listdir('unprocessed/') if isfile(join('unprocessed/', f))]
+
+print(onlyfiles)
+
+
+# command = f"ffmpeg -i original/all-my-life.mp3 processed/all-my-life.mp3"
+# os.system(command)
